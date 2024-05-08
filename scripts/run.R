e5c24558b98d8ae3f94c27c59e4726f6372dd886
Tien-Cheng <39258210+Illustratien@users.noreply.github.com>
2024-05-08 16:07:29 +0200

    update run

--- scripts/run.R
+++ scripts/run.R
@@ -1,12 +1,10 @@
-code_dir <- "scripts/pre-processing"
 # install necessary packages
 source(file.path("scripts/pre-processing","set_up.R"))
-
 # combined raw data and remove outlier
 source(file.path("scripts/pre-processing","data_cleaning.R"))
-
 # combined management files
 source(file.path("scripts/pre-processing","extract_management.R"))
-
 # visualization and overview
 source(file.path("scripts/pre-processing","data_check.R"))
+source(file.path("scripts/pre-processing","generate_map.R"))
+# source(knitr::purl("README.Rmd", quiet=TRUE))
