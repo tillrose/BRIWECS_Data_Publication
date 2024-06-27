5d24a770f9f5c9dd0404ee7325afd06b6f67e28e
wangtien <r05621114@g.ntu.edu.tw>
2024-06-27 14:18:08 +0200

    update run

--- scripts/run.R
+++ scripts/run.R
@@ -4,7 +4,3 @@ source(file.path("scripts/pre-processing","set_up.R"))
 source(file.path("scripts/pre-processing","data_cleaning.R"))
 # combined management files
 source(file.path("scripts/pre-processing","extract_management.R"))
-# visualization and overview
-source(file.path("scripts/pre-processing","data_check.R"))
-source(file.path("scripts/pre-processing","generate_map.R"))
-# source(knitr::purl("README.Rmd", quiet=TRUE)) # will generate Readme.R
